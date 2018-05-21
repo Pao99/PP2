40361a73647e9bcee8eac5592ac20095bccb6bf4
Pao99 <38928180+Pao99@users.noreply.github.com>
2018-05-20 23:31:00 -0600

    Update Inicio.java

--- Inicio/src/Inicio.java
+++ Inicio/src/Inicio.java
@@ -11,7 +11,8 @@ import javax.swing.JOptionPane;
 /**
  *
  * @author Felipe
- */
+ 
+ ñmñkbkjvjcgj
 public class Inicio extends javax.swing.JFrame {
     public static int ventanillasperecederos = 0;
     public static int ventanillasnoperecederos = 0;
